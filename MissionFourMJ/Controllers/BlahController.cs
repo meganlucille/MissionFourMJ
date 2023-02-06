d82d7086fa52bffb3b4bbe33014f96917bb0ce64
Megan Johnson <megan@mightyjohnsons.com>
2023-02-05 20:24:46 -0700

    Mission 5 - MVC

---
+++ MissionFourMJ/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MissionFourMJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionFourMJ.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MissionFourMJ/Models/GradeCalculatorModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionFourMJ.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100)] // Validation in the model to ensure the user enters a number between 0 and 100
+        public byte Assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public byte Projects { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public byte Quizzes { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public byte Midterm { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public byte Final { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public byte Intex { get; set; }
+    }
+}
